7b75b21ecd7ca29befe7d69fe3572bb658d55689
Victoria Gregory <vgregory757@gmail.com>
2016-09-14 16:30:02 -0400

    More new additions

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -112,7 +112,9 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+	'code_font_size': '0.8em',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
