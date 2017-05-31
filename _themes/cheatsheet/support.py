ccff55ecbfa08bfa32074591397af6dc2c916c29
Natasha <tashawatkins@gmail.com>
2017-05-31 13:06:03 +1000

    add stata pygments to stats cheatsheet (#41)
    * add stata pygments to stats cheatsheet
    
    * fix import statement typo

--- _themes/cheatsheet/support.py
+++ _themes/cheatsheet/support.py
@@ -2,3 +2,4 @@ from pygments.style import Style
 from pygments.token import Keyword, Name, Comment, String, Error, \
      Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
 from pygments.lexers.r import SLexer
+from pygments.lexers.stata import StataLexer
